108a4896263a88663d676a61e7b4aea705b7776b
Ondinne <j.padillag@uniandes.edu.co>
2022-10-22 22:10:45 -0500

    Delete task done

--- vistas/task_view.py
+++ vistas/task_view.py
@@ -92,7 +92,7 @@ def error_handler(request, exc, traceback):
         request.id, exc, traceback))
 
 
-@task_view.route("/tasks/<int:id_task>", methods=["GET"])
+@task_view.route('/tasks/<int:id_task>', methods=['GET'])
 @jwt_required()
 def get(id_task):
     task = Task.query.filter(Task.id == id_task).first()
@@ -126,3 +126,15 @@ def get(id_task):
         return jsonify(response)
     else:
         return {"mensaje": "el id_task no existe!"}, http.HTTPStatus.INTERNAL_SERVER_ERROR.value
+
+
+@task_view.route('/tasks/<int:id_task>', methods=['DELETE'])
+@jwt_required()
+def delete(id_task):
+    user_id = get_jwt_identity()
+    task = Task.query.filter(Task.id == id_task).first()
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return '', 204
