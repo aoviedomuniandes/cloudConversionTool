f588c0827f7e91d0b8472c9ea6e40707fb3a23d6
Andres Oviedo <a.oviedom@uniandes.edu.co>
2022-11-09 00:18:48 -0500

    add task , process task , download fix

---
+++ helper/gcloud.py
@@ -0,0 +1,37 @@
+import tempfile
+from pathlib import Path
+
+
+import google.cloud.storage as storage
+
+
+class GCloudClient:
+    _DEFAULT_BUCKET = "cloudconversiontools-bucket"
+
+    def upload_from_file_to_bucket(self, blob_name, path_to_file, bucket_name: str = _DEFAULT_BUCKET):
+        storage_client = storage.Client.from_service_account_json('miso-grupo-2.json')
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_file(path_to_file)
+        return blob.name
+
+    def upload_from_filename_to_bucket(self, blob_name, path_to_file, bucket_name: str = _DEFAULT_BUCKET):
+        storage_client = storage.Client.from_service_account_json('miso-grupo-2.json')
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_filename(path_to_file)
+        return blob.name
+
+    def download_file_to_bucket(self, resource_name, bucket_name: str = _DEFAULT_BUCKET):
+        storage_client = storage.Client.from_service_account_json('miso-grupo-2.json')
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(resource_name)
+        str_path = "{}/{}".format(tempfile.gettempdir(), resource_name)
+        blob.download_to_filename(str_path)
+        return Path(str_path)
+
+    def delete_file_to_bucket(self,resource_name, bucket_name: str = _DEFAULT_BUCKET):
+        storage_client = storage.Client.from_service_account_json('miso-grupo-2.json')
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(resource_name)
+        blob.delete()       
